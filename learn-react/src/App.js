9b764611597b9c8f56cf1a9fbafe838bb137256a
hmirzaa <hamzashazad2001@gmail.com>
2020-10-24 17:02:44 +0500

    Hooks Lec 2

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
    
-import ClassCounter from './hooks/ClassCounter'
-import HookCounter from './hooks/HookCounter'
+// import ClassCounter from './hooks/ClassCounter'
+// import HookCounter from './hooks/HookCounter'
+// import HookCounterTwo from './hooks/HookCounterTwo'
+// import HookCounterThree from './hooks/HookCounterFour'
+import HookCounterFour from './hooks/HookCounterFour'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <center>
-            <ClassCounter />
-            <HookCounter />
+           <HookCounterFour />
         </center>
       </div> 
     )
