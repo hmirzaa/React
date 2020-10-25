1e08d5f6745b74069e12500dbd9c3c9ce5734df1
hmirzaa <hamzashazad2001@gmail.com>
2020-10-25 16:40:47 +0500

    Admin Lte Integration

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -4,15 +4,14 @@ import React, { Component } from 'react'
 // import HookCounter from './hooks/HookCounter'
 // import HookCounterTwo from './hooks/HookCounterTwo'
 // import HookCounterThree from './hooks/HookCounterFour'
-import HookCounterFour from './hooks/HookCounterFour'
+// import HookCounterFour from './hooks/HookCounterFour'
+ import Content from './comp/Content'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <center>
-           <HookCounterFour />
-        </center>
+        <Content />
       </div> 
     )
   }
